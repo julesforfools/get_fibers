1c85edd177659c4c7bbdc174027c8b44cd8bcbbf
julesforfools <45745073+julesforfools@users.noreply.github.com>
2021-01-04 11:07:25 +0900

    Update get-fibers.py
    Update to version 1.2
    - added flipped vectors for oppositely inclined fibers
    - added discrete values for SCALE_MULT based on mm or um
    - fixed error in flipped angle calculation to "angle = abs(angle - 180)"

--- get-fibers.py
+++ get-fibers.py
@@ -1,4 +1,3 @@
-
 import sys
 from mathutils import Vector
 import bpy
@@ -6,39 +5,29 @@ import math
 import os
 
 print(sys.argv[0:])
-#i = 0
-#for a in sys.argv:
-#    print(a,i)
-#    i+=1
-#sys.exit(-1)
-#--output
-#../blender-2.80/blender 0
-#start-fibers.blend 1
-#-P 2
-#read-fibers.py 3
-#-- 4
-#lines-fibers.swc 5
 
-
-#Blended 2.79 not supported!
+#Blender 2.79 not supported!
+#Blender 2.81 supported!
 #----------------------------------------------------------------------------------------------------------------
 # run line:
 # ../blender-2.80/blender start-fibers.blend -P read-fibers.py -- markers-input.txt lines-fibers.swc
 #----------------------------------------------------------------------------------------------------------------
 
-
-
-
-#FILE_FIBERS="lines-fibers.swc"
+#sys.argv is a list in Python, which contains the command-line arguments passed to the script.
+#sys.argv[0] is the directory, sys.argv[1] is the script, etc..
 FILE_MARKERS = sys.argv[4]
 FILE_FIBERS = sys.argv[5]
+SCALE_MULT = sys.argv[6]
+### Set the scale for visualization in Blender ###
+if SCALE_MULT == 'mm':
+    SCALE_MULT = 0.01
+if SCALE_MULT == 'mi':
+    SCALE_MULT = 10
 
-SCALE_MULT = 10
 
-
-
+### Read Apodeme/Tendon Data from 4 landmarks ###
 def ReadApodemeData(filename):
-    p1,p2,p3,p4 = Vector((0,0,0)), Vector((0,0,0)), Vector((0,0,0)), Vector((0,0,0))
+    p1,p2,p3,p4 = Vector((0,0,0)), Vector((0,0,0)), Vector((0,0,0)), Vector((0,0,0)) #p1,p2 are the apodeme/tendon landmarks at the origin. p3,p4 are the apodeme/tendon landmarks along the line of action.
     lines = []
     with open(filename, 'r') as f:
         lines = f.readlines()
@@ -53,14 +42,19 @@ def ReadApodemeData(filename):
                 points = []
                 for pd in pointData:
                     points.append(pd.split(' '))
-                p1 = Vector((float(points[0][0]),float(points[0][1]),float(points[0][2])))*SCALE_MULT
-                p2 = Vector((float(points[1][0]),float(points[1][1]),float(points[1][2])))*SCALE_MULT
-                p3 = Vector((float(points[2][0]),float(points[2][1]),float(points[2][2])))*SCALE_MULT
-                p4 = Vector((float(points[3][0]),float(points[3][1]),float(points[3][2])))*SCALE_MULT
+                p1 = Vector((float(points[0][0]),float(points[0][1]),float(points[0][2])))#*SCALE_MULT
+                p2 = Vector((float(points[1][0]),float(points[1][1]),float(points[1][2])))#*SCALE_MULT
+                p3 = Vector((float(points[2][0]),float(points[2][1]),float(points[2][2])))#*SCALE_MULT
+                p4 = Vector((float(points[3][0]),float(points[3][1]),float(points[3][2])))#*SCALE_MULT
     return p1,p2,p3,p4
 
+def GetApodemeDirection(p1,p2,p3,p4):
+    v12mid = (p1+p2)/2 #Vector(( (p1.x+p2.x)/2.0, (p1.y+p2.y)/2.0, (p1.z+p2.z)/2.0 ))
+    v34mid = (p3+p4)/2 #Vector(( (p3.x+p4.x)/2.0, (p3.y+p4.y)/2.0, (p3.z+p4.z)/2.0 ))
+    return v12mid - v34mid, v12mid, v34mid
 
 
+### Read Muscle Fiber Data from swc-file ###
 def ReadFiberData(filename):
     textlines=[]
     with open(filename, 'r') as f:
@@ -80,19 +74,13 @@ def ReadFiberData(filename):
         pack.append(data)
     return lines
 
-
-def GetApodemeDirection(p1,p2,p3,p4):
-    v12mid = (p1+p2)/2 #Vector(( (p1.x+p2.x)/2.0, (p1.y+p2.y)/2.0, (p1.z+p2.z)/2.0 ))
-    v34mid = (p3+p4)/2 #Vector(( (p3.x+p4.x)/2.0, (p3.y+p4.y)/2.0, (p3.z+p4.z)/2.0 ))
-    return v12mid - v34mid, v12mid, v34mid
-
-
+### Create Points from landmarks ###
 def CreatePointAtLocation(location,size=0.1):
     #scene = bpy.context.scene
-    bpy.ops.mesh.primitive_cube_add(size=size * 0.01 * SCALE_MULT, calc_uvs=True,location=location)
+    bpy.ops.mesh.primitive_cube_add(size=size, calc_uvs=True,location=location) #size=size * 0.01 * SCALE_MULT
     #cube = bpy.context.active_object
 
-
+### Create Curves from apodeme data ###
 def CreateCurve(dataPoints,thickness,color,use_cyclic):
     # create the Curve Data object
     curveData = bpy.data.curves.new('myCurveData', type='CURVE')
@@ -109,7 +97,7 @@ def CreateCurve(dataPoints,thickness,color,use_cyclic):
     for p in dataPoints:                                    # open the loop. for each index(i) and tuple(coord)
         polyline.points[i].co = Vector((p.x, p.y, p.z,1))  # assign to the point at index, the corresponded x,y,z
         i+=1
-    curveData.splines[0].use_cyclic_u = use_cyclic      # specify if the curve cyclic or not
+    curveData.splines[0].use_cyclic_u = use_cyclic      # specify if the curve is cyclic or not
     curveData.splines[0].use_endpoint_u = True          # draw including endpoints
     curveOBJ = bpy.data.objects.new('myCurve', curveData) # crete new curve obj with the curveData
     scene = bpy.context.scene                            # get reference to our scene
@@ -129,8 +117,8 @@ def CreateCurve(dataPoints,thickness,color,use_cyclic):
 def CreateFiberFromTextData(pack):
     lsPoints = []
     for d in pack:
-        point = Vector((float(d[2]),float(d[3]),float(d[4]))) * SCALE_MULT
-        print(point)
+        point = Vector((float(d[2]),float(d[3]),float(d[4])))# * SCALE_MULT
+        #print(point)
         lsPoints.append(point)
     length = 0
     if len(lsPoints) > 2:
@@ -141,46 +129,64 @@ def CreateFiberFromTextData(pack):
 
 
 
-#.2.Get the fiber direction ()
+#### Get the fiber direction ###
 def GetFiberDirection(fiberPoints):
-    vecs=[]
+    vecs=[] #Create Empty list of vectors
     for i in range(0, len(fiberPoints)-1):
         p0 = fiberPoints[i]
         p1 = fiberPoints[i + 1]
-        v = p1 - p0
+        v = p1 - p0 #Directional vector from next to current point
         vecs.append(v)
-        #CreateCurve([p0, p1] , 0.1, (0,255,0,255), False)
     vdir = Vector((0,0,0))
     for v in vecs:
         vdir += v
     vdir = vdir/len(vecs)
+    #CreateCurve([p0, p1] , 0.1, (0,255,0,255), False)
     return vdir.normalized()
 
-#.3.Compute the angle between fiber direction and the apodeme direction. & draw it
+#--------------------------------------------------------------------------
+#### Adjust the scene for tomography data ####
+#--------------------------------------------------------------------------
+#The units are not carried over correctly, so what has been at an x-position of 1000 micrometers would now be at 1000 m, which is out of bounds for the Blender viewer standard
+#The next bit expands the viewer area
 
-#.4.Produce the csv file for the reference
+#bpy.context.scene.unit_settings.scale_length = 1e-05
+#bpy.context.scene.unit_settings.length_unit = 'MICROMETERS'
+#bpy.context.space_data.clip_end = 10000 * SCALE_MULT
+
+for SCREEN_AREA in bpy.context.screen.areas:
+    if SCREEN_AREA.type == 'VIEW_3D':
+        break
+
+SCREEN_AREA.spaces.active.clip_end = 10000 * SCALE_MULT
+
+#Delete the default objects
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.delete(use_global=False)
+
 
 #--------------------------------------------------------------------------
-#automate file input and image/scene generation via blender/python console
+#Automated file input and image/scene generation via blender/python console
 #--------------------------------------------------------------------------
 
+
+####Read and draw the apodeme####
 p1,p2,p3,p4 = ReadApodemeData(FILE_MARKERS)
 
-#darta verts points
+#data verts points in Blender
 CreatePointAtLocation(p1)
 CreatePointAtLocation(p2)
 CreatePointAtLocation(p3)
 CreatePointAtLocation(p4)
 
-
 directionVector, v12mid, v34mid = GetApodemeDirection(p1,p2,p3,p4)
 #apodeme - directional
-CreateCurve([v34mid,v12mid], 0.1 * 0.01 * SCALE_MULT, (0,255,0,255), False)
-#normilized apodeme
+CreateCurve(dataPoints = [v34mid,v12mid], thickness = SCALE_MULT, color = (0,255,0,255), use_cyclic = False)#thickness = 10 * SCALE_MULT
+#normalized apodeme
 normalApodeme = directionVector.normalized()
-CreateCurve([Vector((0,0,0)),normalApodeme], 0.1 * 0.01 * SCALE_MULT, (0,255,0,255), False)
+CreateCurve([Vector((0,0,0)),normalApodeme * 100 * SCALE_MULT], SCALE_MULT, (0,255,0,255), False)#thickness = 10 * SCALE_MULT
 
-
+####Read and draw the fibers, calculate the angles####
 #for each fiber:
 filepath = bpy.data.filepath
 directory = os.path.dirname(filepath)
@@ -193,22 +199,33 @@ for i in range(0, len(allFiberlines)):
     lines = allFiberlines[i]
     #create fiber from data
     fiberPoints, length = CreateFiberFromTextData(lines)
-    #if we got enough poinst:
+    #if we got enough points:
     if len(fiberPoints):
-        CreateCurve(fiberPoints, 0.1 * 0.01 * SCALE_MULT, (255,128,0,255), False)
+        #individual fiber - directional
+        CreateCurve(fiberPoints, SCALE_MULT, (255,128,0,255), False)
         fiberDirection = GetFiberDirection(fiberPoints)
-        #create direction curve
-        CreateCurve([fiberPoints[0], fiberPoints[0]+fiberDirection*length], 0.1 * 0.01 * SCALE_MULT, (255,0,0,255), False)
-        #draw nomalized fiber at origin for debug:
-        CreateCurve([Vector((0,0,0)),fiberDirection], 0.1 * 0.01 * SCALE_MULT, (255,0,0,255), False)
         #compute angle
         angle = math.degrees(fiberDirection.angle(normalApodeme, Vector((0,0,0))))
+        if angle >= 90:
+            angle = abs(angle - 180)
+            #append for output to *.csv
+            rawDirections.append(angle)
+            #create direction curve flipped
+            CreateCurve([fiberPoints[0], fiberPoints[0]+fiberDirection*length], SCALE_MULT, (255,0,0,255), False)
+            #draw nomalized fiber at origin for debug and flipped:
+            CreateCurve([Vector((0,0,0)),-(fiberDirection * 100 * SCALE_MULT)], SCALE_MULT, (255,0,0,255), False)
+        else:
+            #append for output to *.csv
+            rawDirections.append(angle)
+            #create direction curve
+            CreateCurve([fiberPoints[0], fiberPoints[0]+fiberDirection*length], SCALE_MULT, (255,0,0,255), False)
+            #draw nomalized fiber at origin for debug:
+            CreateCurve([Vector((0,0,0)),fiberDirection * 100 * SCALE_MULT], SCALE_MULT, (255,0,0,255), False)
 
-        #append for output to *.csv
-        rawDirections.append(angle)
 
-
-# write directions to one column - csv
+#--------------------------------------------------------------------------
+# Write attachment angles to one column - csv
+#--------------------------------------------------------------------------
 FILE_DIRECTIONS =  os.path.join(directory,"out-directions.csv")
 strDirections=[str(s) for s in rawDirections]
 with open(FILE_DIRECTIONS,'w') as f:
