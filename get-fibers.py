2bbafdea203d389aafd18741a1ea054856417649
julesforfools <45745073+julesforfools@users.noreply.github.com>
2021-04-13 19:35:17 +0900

    Added in new version of recolor_by_angle

--- get-fibers.py
+++ get-fibers.py
@@ -7,7 +7,8 @@ import os
 print(sys.argv[0:])
 
 #Blender 2.79 not supported!
-#Blender 2.81 supported!
+#Blender 2.80-2.91 not supported!
+#Blender 2.92 supported! (New Attribute shader node)
 #----------------------------------------------------------------------------------------------------------------
 # run line:
 # ../blender-2.80/blender start-fibers.blend -P read-fibers.py -- markers-input.txt lines-fibers.swc
@@ -158,6 +159,7 @@ class OBJECT_OT_recolor_by_angle(bpy.types.Operator):
 
     # execute function
     def execute(self, context):
+
         # create the gradient material
         if not "Gradient_Angle" in bpy.data.materials:                          # If material doesn't exist yet,
             bpy.data.materials.new("Gradient_Angle")                            # create it
@@ -166,6 +168,7 @@ class OBJECT_OT_recolor_by_angle(bpy.types.Operator):
         mat.diffuse_color = (100,255,0,255)                                     # Assign Viewport material color
         tree = mat.node_tree                                                    # Assign node tree to variable
         nodes = tree.nodes                                                      # Assign nodes to variable
+        #list(bpy.data.materials["Gradient_Angle"].node_tree.nodes)
 
         # assign nodes to variables
         node_bsdf = nodes['Principled BSDF']
@@ -173,37 +176,45 @@ class OBJECT_OT_recolor_by_angle(bpy.types.Operator):
         node_ramp = nodes['ColorRamp']
         nodes.new('ShaderNodeMath')
         node_math = nodes['Math']
-        nodes.new('ShaderNodeValue')
-        node_value = nodes['Value']
+        #nodes.new('ShaderNodeValue')
+        #node_value = nodes['Value']
+        nodes.new('ShaderNodeAttribute')
+        node_attribute = nodes['Attribute']
 
         # link the nodes together, bsdf is already linked to Output node
         tree.links.new(node_ramp.outputs['Color'], node_bsdf.inputs['Base Color']) # Connect ColorRamp and BSDF
         tree.links.new(node_math.outputs['Value'], node_ramp.inputs[0]) # Connect Math and ColorRamp
-        #tree.links.new(node_value.outputs['Value'], node_math.inputs[0]) # Connect Value and Math, not yet supported
+        #tree.links.new(node_value.outputs['Value'], node_math.inputs[0]) # Connect Value and Math, not supported
+        tree.links.new(node_attribute.outputs[2], node_math.inputs[0])
 
         # customize the nodes
         # color ramp Blue/Red gradient
         node_ramp.color_ramp.elements[0].color = (0,0,1,1)
         node_ramp.color_ramp.elements[1].color = (1,0,0,1) # Don't use RGB 0/255 here, it fs up the ramp
+        # color ramp sets stops at min and max angles for clearer visualization
+        angles = []
+        for ob in bpy.data.collections['Straightened'].all_objects:
+            angles.append(ob.data["attachment_angle"])
+        node_ramp.color_ramp.elements[0].position = min(angles)/90
+        node_ramp.color_ramp.elements[1].position = max(angles)/90
         # Math module division and by 90
         node_math.operation = "DIVIDE"
-        node_math.inputs[1].default_value = 90 # Assume muscle fibers attach at not more than 90 degrees
-        # Give Value an input!
-        node_value.outputs[0].default_value = 0
-        driv = node_value.outputs[0].driver_add("default_value")
-        driv.driver.expression = "100" #needs to link to Object's "attachment_angle" property
-        print(driv.driver.expression)
-        #bpy.data.materials['Gradient_Angle'].users
+        node_math.inputs[1].default_value = 90
+        # Give Value an input # not supported
+        # node_value.outputs[0].default_value = 0
+        # driv = node_value.outputs[0].driver_add("default_value")
+        # driv.driver.expression = "100"
+        # Give Attribute an input
+        node_attribute.attribute_type = 'OBJECT'
+        node_attribute.attribute_name = 'attachment_angle'
 
-        # placeholder for value node: Object Info node and pass index
-        nodes.new('ShaderNodeObjectInfo')
-        node_info = nodes['Object Info']
-        tree.links.new(node_info.outputs[2], node_math.inputs[0])
+        # placeholder for attribute node: Object Info node and pass index
+        #nodes.new('ShaderNodeObjectInfo')
+        #node_info = nodes['Object Info']
+        #tree.links.new(node_info.outputs[2], node_math.inputs[0])
 
         # select objects in 'Straightened'
         context.view_layer.active_layer_collection = context.view_layer.layer_collection.children['Straightened']
-
-        # act out the recoloring
         for ob in bpy.data.collections['Straightened'].all_objects:
             if ob.type == "CURVE":
                 ob.select_set(True)
@@ -222,6 +233,7 @@ class SCENE_PT_recolor_by_angle(bpy.types.Panel):
     bl_context = "scene"
 
     def draw(self, context):
+
         layout = self.layout
         scene = context.scene
         collection = context.collection
